4657139569d3e44953cd31612d853bedb50c1a27
Enzo Gabriel <vscode66@gmail.com>
2025-01-21 16:20:09 -0300

    Nova atualização

--- Models/Department.cs
+++ Models/Department.cs
@@ -5,7 +5,7 @@ namespace WebSales.Models
     public class Department
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name  { get; set; }
 
         public Department(int id, string name)
         {
